d6b2b5095ba03f185c245a72b525ccc2809708bc
kirbyiam <kirby.hsu93@gmail.com>
2019-08-06 22:03:10 -0700

    added a delete button

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const todoInputEl = document.querySelector(".todo__input");
 const todoListEl = document.querySelector(".todo__list");
 const todoItemsEls = document.querySelectorAll(".todo_item");
-// console.log(todoItemsEls);
+const deleteButton = document.querySelectorAll(".delete")
+// console.log(deleteButton);
 
+// with a keypress of enter the inputed value will be printed onto the list with a appended delete button
 function addListItem () {
     todoInputEl.addEventListener("keypress", function(event) {
         if(event.keyCode === 13){
-            let newListItem = createListItem (todoInputEl.value)
+            let deleteButton = createDeleteButton ();
+            let newListItem = createListItem (todoInputEl.value);
         //    todoListEl.appendChild(newListItem);
+        newListItem.appendChild(deleteButton);
         todoListEl.insertBefore(newListItem, todoListEl.childNodes[0])
-           todoInputEl.value = ""; 
+           todoInputEl.value = " "; 
         }
     })
 }
@@ -22,21 +26,42 @@ function addListItem () {
 //     }
 // }
 
+// on the click of the target if it contains the class of "todo_item" toggle "done"
 function toggleDone(){
     todoListEl.addEventListener("click", function (event){
         // console.log(event.target); 
         if (event.target.classList.contains("todo_item")){
             event.target.classList.toggle("done"); 
         }
+        
     })
 }
-
+// create a new element li with the text passed into it while setting the class to todo_item
 function createListItem (text) {
     const newListElement = document.createElement("li");
     newListElement.textContent = text;
     newListElement.setAttribute("class", "todo_item");
     return newListElement;
 }
+// create a delete button that will append to the newListItem in the addListItem ()
 
+function createDeleteButton () {
+    const newListButton = document.createElement('button');
+    newListButton.textContent = ' x ';
+    newListButton.setAttribute('class', 'delete');
+    return newListButton;
+}
+
+// copied what toggle does but for a delete button
+
+function deleteItem (){
+    todoListEl.addEventListener('click', function (event){
+        if(event.target.classList.contains('delete')){
+            event.target.parentNode.remove(); 
+        }
+    })
+}
+
+deleteItem();
 toggleDone(); 
 addListItem(); 
